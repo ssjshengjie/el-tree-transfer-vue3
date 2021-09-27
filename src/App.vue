661733a0e7ea654f0600d0b619d98f0e7d2ef91d
sunshengjie <sunshengjie@joygames.cc>
2021-09-27 18:38:18 +0800

    feat:el-tree-transfer

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="text-tree-transfer">
+    <ElTreeTransferCom
+      width="800px"
+      height="500px"
+      from_title="测试"
+      to_title="到测试"
+      v-model:from_data="fromData"
+      v-model:to_data="toData"
+      @addBtn="addBtn"
+      @removeBtn="removeBtn"
+    />
+  </div>
+</template>
+<script lang='ts' setup>
+import { ref } from 'vue'
+import ElTreeTransferCom from './package/ElTreeTransfer/index'
+// import 'el-tree-transfer-vue3/dist/style.css'
+import { transferData } from './data'
+const fromData = ref(transferData.ruleInfo.diff)
+const toData = ref(transferData.ruleInfo.auth)
+const addBtn = (v: any) => {
+  // console.error(v)
+  fromData.value = v
+}
+const removeBtn = (v: any) => {
+  // console.error("to", v)
+  toData.value = v
+}
+</script>
+
+<style lang="scss">
+</style>
+
+
+
---
+++ src/package/ElTreeTransfer/index.ts
@@ -0,0 +1,5 @@
+import { withInstall } from "./src/utils";
+import ElTreeTransfer from "./src/ElTreeTransfer.vue";
+import "./index.d.ts";
+export const ElTreeTransferCom = withInstall(ElTreeTransfer);
+export default ElTreeTransferCom;
